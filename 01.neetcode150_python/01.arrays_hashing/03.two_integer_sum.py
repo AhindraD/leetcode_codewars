7be0b28ba58bec1b969e1a934ab8c0c8a77fe014
Ahindra D <official.ahindra@gmail.com>
2024-10-05 00:08:55 +0530

    two_sum1

--- 01.neetcode150_python/01.arrays_hashing/03.two_integer_sum.py
+++ 01.neetcode150_python/01.arrays_hashing/03.two_integer_sum.py
@@ -1,14 +1,11 @@
 # https: //neetcode.io/problems/two-integer-sum
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        middle_index = (len(nums) // 2)
-        for i in range(0, middle_index + 1):
-            num2 = target - nums[i]
-            print(num2)
-            if (num2 in nums[i+1:]):
-                # print(num2)
-                if nums[i] == num2:
-                    return [i, nums[i+1:].index(num2)+i+1]
-                else:
-                    return [i, nums.index(num2)]
-        return False
+        valIndxMap={}
+        for i,num in enumerate(nums):
+            diff=target-num
+            if diff in valIndxMap:
+                return [valIndxMap[diff],i]
+            else:
+                valIndxMap[num]=i
+        return None
